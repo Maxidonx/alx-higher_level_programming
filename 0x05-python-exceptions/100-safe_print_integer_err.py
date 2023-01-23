b0a06900cc88fad076a03f28f385e935b23fefce
Maxidonx <johnmaxwell927@gmail.com>
2023-01-23 13:09:54 -0800

    Exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -8,5 +8,7 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return (True)
     except (TypeError, ValueError):
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        sys.stderr.write("Exception:{}\n".format(err))
+
         return (False)
+    return value
