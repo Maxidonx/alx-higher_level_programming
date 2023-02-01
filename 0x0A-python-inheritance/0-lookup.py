a9a8dbc2491fcb6bfab5f3c4897c4d2aee5c4dc9
Maxidonx <johnmaxwell927@gmail.com>
2023-02-01 01:04:39 -0800

    Inheritance its self

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+A python function
+'''
+
+
+def lookup(obj):
+    '''
+    Function to display attributes and
+    methods of an object
+    Args:
+        obj
+    Return:
+        A list called list
+    '''
+    list = dir(obj)
+    return 
