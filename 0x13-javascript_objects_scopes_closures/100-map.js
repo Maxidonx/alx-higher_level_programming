44f8b908ecc628fa272b1427b2095d8e75e867cd
Maxwell I. John <113857428+Maxidonx@users.noreply.github.com>
2023-04-13 23:18:38 +0100

    Update 100-map.js
    done

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,11 +1,4 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
-/*
- * A script that imports an array and computes a new array
- * A new list must be created with each value equal to the
- *value of the initial list, multipled by the index in the list
- * Print both the initial list and the new list
- */
-
 console.log(list);
-console.log(list.map((elem, index) => elem * index));
+console.log(list.map((item, index) => item * index));
